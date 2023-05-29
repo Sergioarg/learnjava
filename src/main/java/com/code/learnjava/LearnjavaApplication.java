7233038736fcc01fab91ae1bb873ba26796b349e
Sergioarg <sergio_ramos53@yahoo.com>
2023-05-29 17:35:42 -0500

    Create: Student in Java

--- src/main/java/com/code/learnjava/LearnjavaApplication.java
+++ src/main/java/com/code/learnjava/LearnjavaApplication.java
@@ -18,10 +18,27 @@ public class LearnjavaApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+			createMultipleStundens(studentDAO);
 		};
 	}
 
+	private void createMultipleStundens(StudentDAO studentDAO) {
+
+		System.out.println("Creating multiple students...");
+		Student tempStudent1 = new Student("Paul", "Doe", "sergio@gamil.com");
+		Student tempStudent2 = new Student("Jhon", "Doe", "jhon@gamil.com");
+		Student tempStudent3 = new Student("Bala", "Doe", "bala@gamil.com");
+
+
+		// Saving students objects
+		System.out.println("Saving students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	// ? Method to Student at the moment of running the project
 	private void createStudent(StudentDAO studentDAO) {
 		// Create the student object
 		System.out.println("Creating new student object...");
