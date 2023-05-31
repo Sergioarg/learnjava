9ece9359b7d20a96aff15caba992b8dec6e32f31
sergioarg <sergio_ramos53@yahoo.com>
2023-05-31 11:11:43 -0500

    Change sout

--- src/main/java/com/code/learnjava/LearnjavaApplication.java
+++ src/main/java/com/code/learnjava/LearnjavaApplication.java
@@ -66,7 +66,7 @@ public class LearnjavaApplication {
 
 		List<Student> theStudents = studentDAO.findAll();
 		for (Student tempStudent : theStudents) {
-			System.out.println(tempStudent);
+			System.out.println("Student: " + tempStudent);
 		}
 	}
 
