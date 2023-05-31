d4391df7f71bbfc7ff9f3e537c72f0850615dc87
sergioarg <sergio_ramos53@yahoo.com>
2023-05-31 17:10:37 -0500

    Add handler exception for BAD_REQUEST type

--- src/main/java/com/code/learnjava/rest/StudentRestController.java
+++ src/main/java/com/code/learnjava/rest/StudentRestController.java
@@ -63,7 +63,21 @@ public class StudentRestController {
         // Create a studentErrorResponse
         StudentErrorResponse error = new StudentErrorResponse();
 
+        // Config type HttpStatus.<type>
         error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        // Create a studentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        // Config type HttpStatus.<type>
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage(exc.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
 
