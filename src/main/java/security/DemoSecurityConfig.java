76e6f1da1f5951f7a8db32b68658aaa67615d207
sergioarg <sergio_ramos53@yahoo.com>
2023-06-05 18:26:51 -0500

    Add authorization

--- src/main/java/security/DemoSecurityConfig.java
+++ src/main/java/security/DemoSecurityConfig.java
@@ -2,36 +2,60 @@ package security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 // Add configuration of security
 @Configuration
 public class DemoSecurityConfig {
 
-    // Check why this not create this users.
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+        // Check why this not create this users.
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
 
-        UserDetails jhon = User.builder()
-                .username("jhon")
-                .password("123")
-                .roles("EMPLOYEE")
-                .build();
+                UserDetails jhon = User.builder()
+                                .username("jhon")
+                                .password("123")
+                                .roles("EMPLOYEE")
+                                .build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+                UserDetails mary = User.builder()
+                                .username("mary")
+                                .password("123")
+                                .roles("EMPLOYEE", "MANAGER")
+                                .build();
 
-        UserDetails susan = User.builder()
-                .username("mary")
-                .password("123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+                UserDetails susan = User.builder()
+                                .username("mary")
+                                .password("123")
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                                .build();
 
-        return new InMemoryUserDetailsManager(jhon, mary, susan);
-    }
+                return new InMemoryUserDetailsManager(jhon, mary, susan);
+
+        }
+
+        // Add permissions authorization
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests(
+                                configurer -> configurer
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+                // Use HTTP basic auth
+                http.httpBasic(Customizer.withDefaults());
+                // Disable CSRF - not necesary for REST API
+                http.csrf(csrf -> csrf.disable());
+
+                return http.build();
+        }
 }
